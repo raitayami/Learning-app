d2a9e3ac8e20ba1ef1803caf27e2c54e0f934af1
tayamirai9 <128257364+tayamirai9@users.noreply.github.com>
2023-10-10 10:09:33 +0100

    Initial Commit

---
+++ Learning-app/Learning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_appApp.swift
+//  Learning-app
+//
+//  Created by Tayami Rai on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Learning_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
